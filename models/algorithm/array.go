775e2efd9e612ff033aced08a956a2393ac43309
ouyangjun <ouyangjun@zhangyue.com>
2019-11-09 22:02:03 +0800

    algorithm

--- models/algorithm/array.go
+++ models/algorithm/array.go
@@ -1,9 +1,170 @@
 //数组常见相关算法
 package algorithm
 
+//杨辉三角
+//1、col=0  元素为1
+//2、col=row 元素为1 并不在为当前行余下元素赋值
+func Generate(numRows int) [][]int {
+	if numRows < 0 {
+		return [][]int{}
+	}
+
+	//声明一个二维切片
+	res := make([][]int, numRows)
+	for index := 0; index < numRows; index++ {
+		res[index] = make([]int, index+1)
+	}
+
+	for i := 0; i < numRows; i++ {
+		for j := 0; j < numRows; j++ {
+			if j == i {
+				res[i][j] = 1
+				break
+			} else if j == 0 {
+				res[i][j] = 1
+			} else if i > 0 && j > 0 {
+				res[i][j] = res[i-1][j] + res[i-1][j-1]
+			}
+		}
+	}
+	return res
+}
+
+//螺旋矩阵
+//给定一个包含 m x n 个元素的矩阵（m 行, n 列），请按照顺时针螺旋顺序，返回矩阵中的所有元素
+//定义上、下、左、右四个元素循环处理
+func SpiralOrder(matrix [][]int) []int {
+	//数组元素为空
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return []int{}
+	}
+
+	//请求当前数组的列数、行数
+	col, row := len(matrix), len(matrix[0])
+
+	//定义 上线左右四个角的元素
+	top, bottom, left, right := 0, col-1, 0, row-1
+	count := 0
+
+	nums := col * row
+	res := make([]int, nums)
+
+	for true {
+		//从左到有遍历
+		for i := left; i <= right; i++ {
+			res[count] = matrix[top][i]
+			count++
+		}
+		top++
+
+		if left > right || top > bottom {
+			break
+		}
+		//从上到下遍历
+		for i := top; i <= bottom; i++ {
+			res[count] = matrix[i][right]
+			count++
+		}
+		right--
+		if left > right || top > bottom {
+			break
+		}
+		//从右到做遍历
+		for i := right; i >= left; i-- {
+			res[count] = matrix[bottom][i]
+			count++
+		}
+		bottom--
+		if left > right || top > bottom {
+			break
+		}
+		//从下到上遍历
+		for i := bottom; i >= top; i-- {
+			res[count] = matrix[i][left]
+			count++
+		}
+		left++
+		if left > right || top > bottom {
+			break
+		}
+	}
+	return res
+}
+
+//以对角线方式遍历二维数字
+//1、m-1,n+1；2、m+1,n-1
+//注意索引溢出情况
+func FindDiagonalOrder(matrix [][]int) []int {
+	//数组元素为空
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return []int{}
+	}
+	//请求当前数组的列数、行数
+	col, row := len(matrix), len(matrix[0])
+
+	//初始化元素总数、列数、行数
+	nums, m, n := col*row, 0, 0
+	res := make([]int, nums)
+	flag := true
+
+	//遍历
+	for i := 0; i < nums; i++ {
+		res[i] = matrix[m][n]
+
+		if flag {
+			//m-1,n+1【行-1，列+1】
+			m = m - 1
+			n = n + 1
+		} else {
+			//m+1,n-1【行+1，列-1】
+			m = m + 1
+			n = n - 1
+		}
+
+		//超过索引范围方式处理
+		if m >= col {
+			m = m - 1
+			n = n + 2
+			flag = true
+		} else if n >= row {
+			n = n - 1
+			m = m + 2
+			flag = false
+		}
+
+		if m < 0 {
+			m = 0
+			flag = false
+		} else if n < 0 {
+			n = 0
+			flag = true
+		}
+	}
+	return res
+}
+
+//加一
+func PlusOne(digits []int) []int {
+	num := len(digits)
+	if num < 1 {
+		return digits
+	}
+
+	for i := num - 1; i >= 0; i-- {
+		if digits[i] < 9 {
+			digits[i]++
+			return digits
+		}
+		digits[i] = 0
+	}
+	rs := make([]int, num+1)
+	rs[0] = 1
+	return rs
+}
+
 //数组中求最大的数，并且是其他数的两倍
 //解题思路：暴力破解，进行两层循环比较
-func dominantIndex(nums []int) int {
+func DominantIndex(nums []int) int {
 	numsLen := len(nums)
 	if numsLen < 1 {
 		return -1
@@ -36,7 +197,7 @@ func dominantIndex(nums []int) int {
 }
 
 //解题思路：遍历数组，找到最大值、次大值，最后进行比较
-func dominantIndexV2(nums []int) int{
+func DominantIndexV2(nums []int) int{
 	numsLen := len(nums)
 	if numsLen < 1 {
 		return -1
@@ -63,7 +224,7 @@ func dominantIndexV2(nums []int) int{
 }
 
 //解题思路 假设第一个为最大值
-func dominantIndexV3(nums []int) int{
+func DominantIndexV3(nums []int) int{
 	numsLen := len(nums)
 	if numsLen < 1{
 		return -1
