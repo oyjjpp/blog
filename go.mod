dbd9be79d16d1fc4c411479c98966df62970237a
ouyangjun <ouyangjuncust@aliyun.com>
2020-08-03 08:40:09 +0800

    优雅重启服务

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/oyjjpp/blog
 go 1.13
 
 require (
+	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6
 	github.com/gin-contrib/pprof v1.3.0
 	github.com/gin-gonic/gin v1.6.2
 	github.com/golang/protobuf v1.4.2
--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"syscall"
+	"time"
+
+	"github.com/fvbock/endless"
 	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
 	"github.com/oyjjpp/blog/route"
 )
 
 func main() {
+	endlessCreate()
+}
+
+// ginCreate
+func ginCreate() {
+	endless.DefaultReadTimeOut = 5
 	handler := gin.New()
 	// 注册路由
 	route.LoadRoute(handler)
@@ -16,4 +28,32 @@ func main() {
 	handler.Use(gin.Logger())
 	// 监听端口
 	handler.Run() // 监听并在 0.0.0.0:8080 上启动服务
+}
+
+// endlessCreate
+func endlessCreate() {
+	endless.DefaultReadTimeOut = 5 * time.Second
+	endless.DefaultWriteTimeOut = 5 * time.Second
+	endless.DefaultMaxHeaderBytes = 1 << 20
+	endPoint := fmt.Sprintf(":%d", 8080)
+
+	server := endless.NewServer(endPoint, initRouter())
+
+	server.BeforeBegin = func(add string) {
+		log.Printf("actual pid is %d", syscall.Getpid())
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Printf("server err :%v", err)
+	}
+}
+
+// initRouter
+func initRouter() *gin.Engine {
+	handler := gin.New()
+	handler.Use(gin.Logger())
+	handler.Use(gin.Recovery())
+	// 注册路由
+	route.LoadRoute(handler)
+	return handler
 }
